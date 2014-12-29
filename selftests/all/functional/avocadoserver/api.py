60369fde6d27896933e367079b78adfb5bb568de
Cleber Rosa <crosa@redhat.com>
2014-12-29 12:41:57 -0300

    API functional test: add test for /teststatuses/ post
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/all/functional/avocadoserver/api.py
+++ selftests/all/functional/avocadoserver/api.py
@@ -77,6 +77,12 @@ class api(test.Test):
         self.assertIn("TEST_NA", names)
         self.assertIn("WARN", names)
 
+    def test_teststatus_noadd(self):
+        self.log.info('Testing that the server does not allow adding a new '
+                      'test status')
+        data = {"name": "NEW_STATUS"}
+        self.post("/teststatuses/", data, 403)
+
     def test_jobs_empty(self):
         self.log.info('Testing that the server has no jobs')
         emtpy = {u'count': 0,
@@ -118,6 +124,7 @@ class api(test.Test):
         self.test_jobstatus_list()
         self.test_jobstatus_noadd()
         self.test_teststatus_list()
+        self.test_teststatus_noadd()
         self.test_jobs_empty()
         self.test_jobs_add()
         self.test_jobs_del()
